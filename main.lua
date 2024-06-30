46f406a4f6abfa7a0c1d19cb6c060ff0f5d1a0d4
RealTroyDex <151197474+RealTroyDex@users.noreply.github.com>
2024-06-29 23:09:31 -0300

    remade team check

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+getgenv().teamcheck = true
+
 local ESP = {}
 
 local pls = {}
@@ -5,41 +7,44 @@ local pls = {}
 function ESP.add_table()
     local plr = game.Players.LocalPlayer
     for _, v in pairs(game:GetService("Players"):GetPlayers()) do
-        if v.TeamColor ~= plr.TeamColor then
+        if v ~= plr then
             ESP.add_player(v)
         end
     end
 end
 
 function ESP.add_player(plr)
-    print("Added table to: " .. plr.Name)
-    local char = plr.Character or plr:FindFirstChildWhichIsA("Model")
-    if char then
-        local hl = Instance.new("Highlight", char:FindFirstChild("HumanoidRootPart"))
-        hl.FillColor = Color3.new(1, 0, 0)
-        hl.Adornee = char
-        pls[plr] = {
-            dead = false,
-            Highlight = hl
-        }
-    end
-
-    plr.CharacterAdded:Connect(function(char)
-        local hl = pls[plr] and pls[plr].Highlight
-        if hl then
-            hl.Parent = char:FindFirstChild("HumanoidRootPart")
+    local localPlayer = game.Players.LocalPlayer
+    if not getgenv().teamcheck or (plr.Team ~= localPlayer.Team) then
+        print("Added table to: " .. plr.Name)
+        local char = plr.Character or plr:FindFirstChildWhichIsA("Model")
+        if char then
+            local hl = Instance.new("Highlight", char:FindFirstChild("HumanoidRootPart"))
+            hl.FillColor = Color3.new(1, 0, 0)
             hl.Adornee = char
-            print("Re-attached highlight to: " .. plr.Name)
-        else
-            local newHL = Instance.new("Highlight", char:FindFirstChild("HumanoidRootPart"))
-            newHL.FillColor = Color3.new(1, 0, 0)
-            newHL.Adornee = char
             pls[plr] = {
-                Highlight = newHL
+                dead = false,
+                Highlight = hl
             }
-            print("Making highlight for: " .. plr.Name)
         end
-    end)
+
+        plr.CharacterAdded:Connect(function(char)
+            local hl = pls[plr] and pls[plr].Highlight
+            if hl then
+                hl.Parent = char:FindFirstChild("HumanoidRootPart")
+                hl.Adornee = char
+                print("Re-attached highlight to: " .. plr.Name)
+            else
+                local newHL = Instance.new("Highlight", char:FindFirstChild("HumanoidRootPart"))
+                newHL.FillColor = Color3.new(1, 0, 0)
+                newHL.Adornee = char
+                pls[plr] = {
+                    Highlight = newHL
+                }
+                print("Making highlight for: " .. plr.Name)
+            end
+        end)
+    end
 end
 
 ESP.add_table()
