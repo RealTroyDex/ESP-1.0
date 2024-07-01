50ff863f3fe19bfc1723df101fb5e6c1d78564b6
RealTroyDex <151197474+RealTroyDex@users.noreply.github.com>
2024-07-01 11:38:13 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,75 +1,18 @@
-local ESP = {}
+local esp = {}
 
-function ESP.main()
-    local pls = {}
+local plr = game.Players.LocalPlayer
+local plrs = {}
 
-    function ESP.add_table()
-        local plr = game.Players.LocalPlayer
-        for _, v in pairs(game:GetService("Players"):GetPlayers()) do
-            if v.TeamColor ~= plr.TeamColor then
-                ESP.add_player(v)
-            end
-        end
+local players = game:GetService("Players")
+
+for _, v in pairs(players:GetPlayers()) do
+    if v ~= plr then
+        table.insert(plrs, v)
     end
-
-    function ESP.add_player(plr)
-        print("Added table to: " .. plr.Name)
-        local char = plr.Character or plr:FindFirstChildWhichIsA("Model")
-        if char then
-            local hl = Instance.new("Highlight", char:FindFirstChild("HumanoidRootPart"))
-            hl.FillColor = Color3.new(1, 0, 0)
-            hl.Adornee = char
-            pls[plr] = {
-                dead = false,
-                Highlight = hl
-            }
-        end
-
-        plr.CharacterAdded:Connect(function(char)
-            local hl = pls[plr] and pls[plr].Highlight
-            if hl then
-                hl.Parent = char:FindFirstChild("HumanoidRootPart")
-                hl.Adornee = char
-                print("Re-attached highlight to: " .. plr.Name)
-            else
-                local newHL = Instance.new("Highlight", char:FindFirstChild("HumanoidRootPart"))
-                newHL.FillColor = Color3.new(1, 0, 0)
-                newHL.Adornee = char
-                pls[plr] = {
-                    Highlight = newHL
-                }
-                print("Making highlight for: " .. plr.Name)
-            end
-        end)
-    end
-
-    ESP.add_table()
-
-    game:GetService("Players").PlayerAdded:Connect(function(plr)
-        ESP.add_player(plr)
-    end)
-
-    game:GetService("Players").PlayerRemoving:Connect(function(plr)
-        local plrData = pls[plr]
-        if plrData then
-            plrData.Highlight:Destroy()
-            pls[plr] = nil
-            print("Removed highlight for: " .. plr.Name)
-        end
-    end)
-
-    game:GetService("RunService").RenderStepped:Connect(function()
-        for plr, plrData in pairs(pls) do
-            if plrData.Highlight.Parent == nil then
-                local char = plr.Character or plr:FindFirstChildWhichIsA("Model")
-                if char then
-                    plrData.Highlight.Parent = char:FindFirstChild("HumanoidRootPart")
-                    plrData.Highlight.Adornee = char
-                    print("Reattached highlight to: " .. plr.Name)
-                end
-            end
-        end
-    end)
 end
 
-return ESP
+for _, v in pairs(plrs) do
+    print("Player: "..v)
+end
+
+return esp
