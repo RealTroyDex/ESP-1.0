188cafef536b39e24193d4a08a4b605795f6edf6
RealTroyDex <151197474+RealTroyDex@users.noreply.github.com>
2024-07-01 11:46:26 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,11 @@
 local ESP = {}
 
 local plr = game.Players.LocalPlayer
-local plrs = {}
 
 local players = game:GetService("Players")
 
 function ESP.see()
+    local plrs = {}
     for _, v in pairs(players:GetPlayers()) do
         if v ~= plr then
             table.insert(plrs, v)
