7db3fba4811af47439f07be6260d012987e40012
RealTroyDex <151197474+RealTroyDex@users.noreply.github.com>
2024-07-01 12:04:13 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,24 +4,50 @@ local plr = game.Players.LocalPlayer
 
 local players = game:GetService("Players")
 
-function ESP.see(printable)
+local plrs = {}
+
+function ESP.addplayers(printable)
 
     if printable == nil then
         print('Argument at function "ESP.see()" is missing.')
     end
 
-    local plrs = {}
+    
     for _, v in pairs(players:GetPlayers()) do
-        if v ~= plr then
+        if v ~= plr and table.find(plrs, v) == nil then
             table.insert(plrs, v)
         end
     end
     
     if printable then
         for _, v in pairs(plrs) do
-            print("Player: "..v)
+            print("Player added to table: "..v)
         end
     end
 end
+
+function ESP.remove(player, printable)
+    for i,v in pairs(plrs) do
+        if table.find(plrs, player) ~= nil then
+            table.remove(plrs, i)
+        end
+    end
+end
+
+function ESP.onJoin(player)
+    if player then
+        addplayers(true)
+    end
+end
+
+players.PlayerAdded:Connect(function(player)
+    if player then
+        ESP.onJoin(player)
+    end
+end)
+
+players.PlayerRemoving:Connect(function(player)
+    ESP.remove(player, true)
+end)
 
 return ESP
