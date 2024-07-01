4851d59e4e3c8898a720b48257f15c756b36fff9
RealTroyDex <151197474+RealTroyDex@users.noreply.github.com>
2024-07-01 11:53:01 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,7 +4,12 @@ local plr = game.Players.LocalPlayer
 
 local players = game:GetService("Players")
 
-function ESP.see()
+function ESP.see(printable)
+
+    if printable == nil then
+        print('Argument at function "ESP.see()" is missing.')
+    end
+
     local plrs = {}
     for _, v in pairs(players:GetPlayers()) do
         if v ~= plr then
@@ -12,8 +17,10 @@ function ESP.see()
         end
     end
     
-    for _, v in pairs(plrs) do
-        print("Player: "..v.Name)
+    if printable then
+        for _, v in pairs(plrs) do
+            print("Player: "..v)
+        end
     end
 end
 
