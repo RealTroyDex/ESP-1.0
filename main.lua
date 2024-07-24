ecc7f0062c842aa8749e58850cb237165756d25e
RealTroyDex <151197474+RealTroyDex@users.noreply.github.com>
2024-07-24 17:17:25 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,83 +1,38 @@
--- Made by TroyDex
-
--- Simple Roblox ESP Module
+-- ESP 1.0 (Trash)
+-- Remade because yes?
 
 local ESP = {}
 
 local plr = game.Players.LocalPlayer
-local players = game:GetService("Players")
-local plrs = {}
 
-local function addhighlight(plr, teamcheck)
-    if plr.Character and plr.Character:FindFirstChild("Highlight") == nil and teamcheck == true then
-        local highlight = Instance.new("Highlight")
-        highlight.Adornee = plr.Character
-        highlight.FillColor = plr.TeamColor.Color
-        highlight.OutlineColor = Color3.new(0, 0, 0)
-        highlight.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
-        highlight.Parent = plr.Character
-    else
-        local highlight = Instance.new("Highlight")
-        highlight.Adornee = plr.Character
-        highlight.FillColor = Color3.new(1, 0, 0)
-        highlight.OutlineColor = Color3.new(0, 0, 0)
-        highlight.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
-        highlight.Parent = plr.Character
-    end
-end
+local players = game.Players:GetPlayers()
 
-function ESP.addplayers(printable, team)
-    if printable == nil then
-        print('Argument at function "ESP.addplayers()" is missing.')
-    end
-
-    for _, v in pairs(players:GetPlayers()) do
-        if v ~= plr and table.find(plrs, v) == nil then
-            table.insert(plrs, v)
-            addhighlight(v, team)
-            
-            v.CharacterAdded:Connect(function()
-                addhighlight(v, team)
-            end)
-        end
-    end
-    
-    if printable then
-        for _, v in pairs(plrs) do
-            print("Player added to table: " .. v.Name)
+-- function to add players to the array
+function ESP.add(player)
+    if player then
+        if player.Character and player.Character:FindFirstChild("Highlight") == nil then
+            local highlight = Instance.new("Highlight")
+            highlight.Adornee = player.Character
+            highlight.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+            highlight.FillColor = Color3.new(1, 0, 0)
+            highlight.OutlineColor = Color3.new(0, 0, 0)
+            highlight.Parent = player.Character
         end
     end
 end
 
-function ESP.remove(player, printable)
-    for i, v in pairs(plrs) do
-        if v == player then
-            table.remove(plrs, i)
-            break
-        end
+function ESP.start()
+    -- Initial start
+    for _, v in pairs(players) do
+        ESP.add(v)
     end
-    if printable then
-        print("Player removed from table: " .. player.Name)
-    end
-end
-
-function ESP.onJoin(player)
-    addhighlight(player)
-    
-    player.CharacterAdded:Connect(function()
-        addhighlight(player)
-    end)
-end
-
-function ESP.main(printable, teamcheck)
-    ESP.addplayers(printable, teamcheck)
-    
-    players.PlayerAdded:Connect(function(player)
-        ESP.onJoin(player)
-    end)
-    
-    players.PlayerRemoving:Connect(function(player)
-        ESP.remove(player, printable)
+    -- connect the event when a player joins the game
+    game.Players.PlayerAdded:Connect(function(player)
+        ESP.add(player)
+        -- connect this function (it triggers when the character dies, so adds the highlight again)
+        player.CharacterAdded:Connect(function()
+            ESP.add(player)
+        end)
     end)
 end
 
